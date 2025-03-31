971b290f342ef819620efac734dc986a8e256854
Velimir <vukashinovich.velimir@gmail.com>
2025-03-31 22:06:26 +0200

    fixed bug with ace high straight, added base for betting functionality, game engine

--- classes.py
+++ classes.py
@@ -47,10 +47,15 @@ class Deck:
         return cards_dealt
             
 class Player():
-    def __init__(self):
+    def __init__(self, id):
+        self.id = id
         self.hand = []
         self.hand_rank = []
         self.best_hand = []
+        self.stack = 1000
+        
+    def get_id(self):
+        return self.id
         
     def add_card(self, cards):
         if len(cards) == 1:
@@ -66,6 +71,21 @@ class Player():
             return f"Your hand: {[str(card) for card in self.hand]}"
         else:
             return self.hand
+        
+    def get_stack(self, to_print=False):
+        if to_print:
+            return f"${self.stack}"
+        else:
+            return self.stack
+        
+    def bet(self, amount):
+        if amount <= self.stack:
+            self.stack -= amount
+        else:
+            raise ValueError("You can't bet more than your stack!")
+        
+    def win(self, amount):
+        self.stack += amount
     
     def set_best_hand(self, value, best_hand):
         self.hand_rank = value
@@ -97,10 +117,43 @@ class Dealer():
             'K' : 13,
             'A' : 14
         }
+
+        self.hand_name = {
+            0 : "High Card",
+            1 : "One Pair",
+            2 : "Two Pair",
+            3 : "Three of a Kind",
+            4 : "Straight",
+            5 : "Flush",
+            6 : "Full House",
+            7 : "Four of a Kind",
+            8 : "Straight Flush",
+            9 : "Royal Flush"
+        }
         self.runout = []
         self.burn_pile = []
         self.pot = 0
         self.deck = Deck()
+        self.bet = 0
+
+    def name_hand(self, value):
+        return self.hand_name[value]
+    
+    def get_bet(self, to_print=False):
+        if to_print:
+            return f"Current bet is: ${self.bet}."
+        else:
+            return self.bet
+        
+    def get_pot(self, to_print):
+        if to_print:
+            return f"The pot is: ${self.pot}."
+        else:
+            return self.pot
+        
+    def set_bet(self, amount):
+        self.pot += amount
+        self.bet = amount
         
     def deal_next(self):
         if len(self.runout) == 5:
@@ -126,8 +179,8 @@ class Dealer():
     
     def find_first_difference_in_hands(self, *hands):
         for i, values in enumerate(zip(*hands)):
-            if len(set(values)) > 1:
-                return i 
+            if len(set([card.value for card in values])) > 1:
+                return i
         return -1
     
     def evaluate_hand(self, hand):
@@ -178,8 +231,9 @@ class Dealer():
                         break
         elif {14, 5, 4, 3, 2}.issubset(values):
             straight = True
-            straight_values = {14, 5, 4, 3, 2}
+            straight_values = {5, 4, 3, 2, 14}
             straight_cards = [card for card in hand if self.valuation[card.get_value()] in straight_values]
+            straight_cards.append(straight_cards.pop(0))
     
         flush_suit = None
         flush_cards = []
@@ -198,6 +252,7 @@ class Dealer():
         if straight and flush and high_card == 14:
             return 9, straight_cards
         if straight and flush:
+            # TODO: add logic for ditinguishing straight flush from 7 card hand that has only straight and only flush.
             return 8, straight_cards
         if four_of_a_kind:
             best_hand = [card for card in hand if self.valuation[card.get_value()] == four_value]
@@ -230,5 +285,75 @@ class Dealer():
     def break_tie(self, players):
         differing_index = self.find_first_difference_in_hands(*[player.get_best_hand() for player in players])
         max_val = max([self.valuation[player.get_best_hand()[differing_index].get_value()] for player in players])
-        return [player for player in players if self.valuation[player.get_best_hand()[differing_index].get_value()] == max_val]
-       
+        winners = [player for player in players if self.valuation[player.get_best_hand()[differing_index].get_value()] == max_val]
+        if len(winners) == 1:
+            return winners
+        else:
+            differing_index = self.find_first_difference_in_hands(*[player.get_best_hand() for player in winners])
+            max_val = max([self.valuation[player.get_best_hand()[differing_index].get_value()] for player in winners])
+            winners_retied = [player for player in winners if self.valuation[player.get_best_hand()[differing_index].get_value()] == max_val]
+            return winners_retied
+        
+class Game:
+    def __init__(self, players, dealer):
+        self.players = players
+        self.dealer = dealer
+
+    def do_round(self, round_number):
+        for i in range(len(self.players)):
+            player = self.players[i]
+            print(f"Player {player.get_id()}:")
+            print(player.get_hand(to_print=True))
+            print(f"Your stack is: {player.get_stack(to_print=True)}.\n")
+
+            all_good = False
+            if round_number == 1:
+                if i == 0:
+                    print("You are the small blind. You automatically bet $1.\n---------------------------")
+                    player.bet(1)
+                    self.dealer.set_bet(1)
+                    all_good = True
+                if i == 1:
+                    print("You are the big blind. You automatically bet $2.\n---------------------------")
+                    player.bet(2)
+                    self.dealer.set_bet(2)
+                    all_good = True
+            else:
+                print(self.dealer.get_runout(to_print=True))
+
+            while all_good == False:
+                player_bet = input(f"{self.dealer.get_pot(to_print=True)}\n{self.dealer.get_bet(to_print=True)}\nHow much do you want to bet?\n")
+                if player_bet.upper() == "C":
+                    if self.dealer.get_bet() == 0:
+                        print("You checked.")
+                    else:
+                        print("You called.")
+                        player.bet(self.dealer.get_bet())
+                        self.dealer.set_bet(self.dealer.get_bet())
+
+                    all_good = True
+                elif player_bet.upper() == "A":
+                    print("You went all in!")
+                    bet_amount = player.get_stack()
+                    self.dealer.set_bet(bet_amount)
+                    player.bet(bet_amount)
+                    all_good = True
+                else:
+                    try:
+                        player_bet = int(player_bet)
+                        print(f"You bet ${player_bet}.\n")
+                        print("---------------------------")
+                        player.bet(player_bet)
+                        self.dealer.set_bet(player_bet)
+                        all_good = True
+                    except ValueError:
+                        print("Illegal bet. Please type a legal bet.")
+
+    def start_game(self):
+        self.do_round(1)
+        self.dealer.deal_next()
+        self.do_round(2)
+        self.dealer.deal_next()
+        self.do_round(3)
+        self.dealer.deal_next()
+        self.do_round(4)
--- main.py
+++ main.py
@@ -1,44 +1,71 @@
-from classes import Player, Dealer
+from classes import Player, Dealer, Game
 
 dealer = Dealer()
 
-players = [Player() for i in range(6)]
+players = [Player(i+1) for i in range(8)]
 
 for i in range(2):
     for player in players:
         player.add_card(dealer.deal_player_card())
-        
-dealer.deal_next()
-dealer.deal_next()
-dealer.deal_next()
 
-for player in players:
-    print(player.get_hand(to_print=True))
-print(dealer.get_runout(to_print=True))
+engine = Game(players, dealer)
 
-max_rank = -1
-tied_players = []
-for player in players:
-    value, best_hand = dealer.evaluate_hand(player.get_hand())
-    player.set_best_hand(value, best_hand)
-    print(value, [str(card) for card in best_hand])
-    if value > max_rank:
-        max_rank = value
-        tied_players = [player]
-    elif value == max_rank:
-        tied_players.append(player)
+engine.start_game()
         
-if len(tied_players) == 1:
-    print("We have a winner!")
-    print(tied_players[0].get_hand_value(), [str(card) for card in tied_players[0].get_best_hand()])
-else:
-    winning_players = dealer.break_tie(tied_players)
-    if len(winning_players) == 1:
-        print("We have a winner!")
-        print(winning_players[0].get_hand_value(), [str(card) for card in winning_players[0].get_best_hand()])
-    elif len(winning_players) > 1:
-        print("We have multiple winners!")
-        for winning_player in winning_players:
-            print(winning_player.get_hand_value(), [str(card) for card in winning_player.get_best_hand()])
-    else:
-        print("other")
+# dealer.deal_next()
+# dealer.deal_next()
+# dealer.deal_next()
+
+# players[0].hand[0].value = "A"
+# players[0].hand[1].value = "2"
+# players[1].hand[0].value = "7"
+# players[1].hand[1].value = "6"
+
+# players[0].hand[0].suit = "♣"
+# players[0].hand[1].suit = "♣"
+# players[1].hand[0].suit = "♣"
+# players[1].hand[1].suit = "♣"
+
+# dealer.runout[0].value = "3"
+# dealer.runout[1].value = "Q"
+# dealer.runout[2].value = "5"
+# dealer.runout[3].value = "K"
+# dealer.runout[4].value = "T"
+
+# dealer.runout[0].suit = "♣"
+# dealer.runout[1].suit = "♣"
+# dealer.runout[2].suit = "♣"
+# dealer.runout[3].suit = "♣"
+# dealer.runout[4].suit = "♣"
+
+
+# for player in players:
+#     print(player.get_hand(to_print=True))
+# print(dealer.get_runout(to_print=True))
+
+# max_rank = -1
+# tied_players = []
+# for player in players:
+#     value, best_hand = dealer.evaluate_hand(player.get_hand())
+#     player.set_best_hand(value, best_hand)
+#     print(value, [str(card) for card in best_hand])
+#     if value > max_rank:
+#         max_rank = value
+#         tied_players = [player]
+#     elif value == max_rank:
+#         tied_players.append(player)
+        
+# if len(tied_players) == 1:
+#     print("We have a winner!")
+#     print(f"Player {tied_players[0].get_id()}, {dealer.name_hand(tied_players[0].get_hand_value())}, {[str(card) for card in tied_players[0].get_best_hand()]}")
+# else:
+#     winning_players = dealer.break_tie(tied_players)
+#     if len(winning_players) == 1:
+#         print("We have a winner!")
+#         print(f"Player {winning_players[0].get_id()}, {dealer.name_hand(winning_players[0].get_hand_value())}, {[str(card) for card in winning_players[0].get_best_hand()]}")
+#     elif len(winning_players) > 1:
+#         print("We have multiple winners!")
+#         for winning_player in winning_players:
+#             print(f" Player {winning_players[0].get_id()}, {dealer.name_hand(winning_player.get_hand_value())}, {[str(card) for card in winning_player.get_best_hand()]}")
+#     else:
+#         print("other")
